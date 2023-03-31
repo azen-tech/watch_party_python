d292c945901ffd9df44ee6f5dd5213ddb394affe
Akshat Jain <82443554+azen-tech@users.noreply.github.com>
2023-03-31 04:23:50 +0000

    modified

--- demo.py
+++ demo.py
@@ -1,2 +1,9 @@
-num = int(input("enter number to square:\t"))
-print("square of",num,"is",num**2)
+def linear_search(arr,num):
+    for i in range(len(arr)):
+        if arr[i] == num:
+            return i
+    return -1
+
+arr=[1,2,3,4,5,6,7,8,9,10]
+num=5
+print(linear_search(arr,num))
