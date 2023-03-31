88f05e9f41b50d5fa5ed54fa2599b66ad006ec45
Akshat Jain <82443554+azen-tech@users.noreply.github.com>
2023-03-31 04:34:33 +0000

    created variables file

---
+++ variables.py
@@ -0,0 +1,22 @@
+# Declare and initialize variables
+name = "John"
+age = 25
+height = 1.75
+is_student = True
+
+# Print the values of variables
+print("Name:", name)
+print("Age:", age)
+print("Is Student:", is_student)
+
+# Update the value of a variable
+age = 26
+
+# Print the updated value of the age variable
+print("Updated Age:", age)
+
+# Type of variables
+print("Type of name:", type(name))
+print("Type of age:", type(age))
+print("Type of height:", type(height))
+print("Type of is_student:", type(is_student))
