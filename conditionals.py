cf084c2c9f1b0338d23ae04d6129739ada51dbf0
Akshat Jain <82443554+azen-tech@users.noreply.github.com>
2023-03-31 05:01:53 +0000

    ch  2  git add conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,8 @@
+traffic_light_color = "red"
+
+if traffic_light_color == "red":
+    print("Stop! The light is red.")
+elif traffic_light_color == "yellow":
+    print("Slow down! The light is yellow.")
+else:
+    print("Go! The light is green.")
---
+++ functions.py
@@ -0,0 +1,20 @@
+# This is a single-line comment in Python
+
+"""
+This is a multi-line comment
+that can span across multiple lines
+"""
+
+def greet(name):
+    # Indentation is used to define blocks of code
+    # In this case, the block of code under this if statement is indented
+    if name == "Alice":
+        print("Hello Alice!")
+    else:
+        print("Hello " + name + "!")
+
+# # This function call will print "Hello Alice!"
+# greet("Alice")
+
+# # This function call will print "Hello Bob!"
+# greet("Bob")
